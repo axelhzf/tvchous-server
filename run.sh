134b8b50565809af39e08e673ccd3bdf253097a6
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-08-06 10:56:25 +0100

    readme

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DOWNLOADED_FOLDER=/Users/axelhzf/Downloads/utorrent/downloaded \
-SHOWS_FOLDER=/Users/axelhzf/Downloads/utorrent/tvshows \
-UTORRENT_USER=axelhzf \
-UTORRENT_PASSWORD=blotts \
-UTORRENT_PORT=40959 \
-ENV=dev \
-node --harmony lib/server.js
